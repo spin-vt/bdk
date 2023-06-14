8ddc4fba55d7188c17abe5928526e47e9c684a77
Vineet Marri <vineet.marri03@gmail.com>
2023-06-14 13:28:12 -0400

    updated Map.js with h3.js library to optimize marker rendering

---
+++ front-end/components/Map.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useRef, useState } from "react";
+import L from "leaflet";
+import "leaflet.markercluster/dist/leaflet.markercluster";
+import "../styles/Map.module.css";
+
+const h3 = require("h3-js");
+
+function Map({ markers }) {
+  const mapRef = useRef(null);
+  const [hexIndexToMarkers, setHexIndexToMarkers] = useState({});
+  const polygonsRef = useRef([]);
+  const markerLayersRef = useRef([]);
+
+  useEffect(() => {
+    // Initialize map on component mount
+    const map = L.map("map");
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map);
+    map.setView([37.0902, -95.7129], 5); // Set view to United States coordinates
+    mapRef.current = map;
+
+    // Cleanup on component unmount
+    return () => {
+      if (map) {
+        map.remove();
+        mapRef.current = null;
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    let newHexIndexToMarkers = {};
+
+    markers.forEach(marker => {
+      const h3Index = h3.latLngToCell(marker.latitude, marker.longitude, 7);
+
+      // Add marker to hex index mapping
+      if (!newHexIndexToMarkers[h3Index]) {
+        newHexIndexToMarkers[h3Index] = [];
+      }
+      newHexIndexToMarkers[h3Index].push(marker);
+    });
+
+    setHexIndexToMarkers(newHexIndexToMarkers);
+  }, [markers]);
+
+  useEffect(() => {
+    const map = mapRef.current;
+
+    Object.keys(hexIndexToMarkers).forEach(h3Index => {
+      const hexBoundary = h3.cellToBoundary(h3Index);
+      const latLngs = hexBoundary.map(coord => L.latLng(coord[0], coord[1]));
+
+      const polygon = L.polygon(latLngs, {
+        color: "blue",
+        fillColor: "blue",
+        fillOpacity: 0.5
+      }).addTo(map);
+
+      polygonsRef.current.push(polygon);
+
+      polygon.on("click", () => {
+        const currentZoom = map.getZoom();
+        if (currentZoom >= 10) {
+          hexIndexToMarkers[h3Index].forEach(marker => {
+            let markerLayer;
+            if (marker.served === true) {
+              markerLayer = L.circleMarker([marker.latitude, marker.longitude], {
+                radius: 5,
+                color: "green",
+                fillColor: "green",
+                fillOpacity: 1
+              }).addTo(map);
+            } else {
+              markerLayer = L.circleMarker([marker.latitude, marker.longitude], {
+                radius: 5,
+                color: "red",
+                fillColor: "red",
+                fillOpacity: 1
+              }).addTo(map);
+            }
+            markerLayersRef.current.push(markerLayer);
+          });
+          polygon.setStyle({ fillOpacity: 0});
+        }
+      });
+    });
+
+    map.on("zoomend", () => {
+      const currentZoom = map.getZoom();
+      if (currentZoom < 10) {
+        polygonsRef.current.forEach(polygon => {
+          polygon.setStyle({ fillOpacity: 0.5 });
+        });
+
+        markerLayersRef.current.forEach(markerLayer => {
+          map.removeLayer(markerLayer);
+        });
+        markerLayersRef.current = [];
+      }
+    });
+
+  }, [hexIndexToMarkers]);
+
+  return (
+    <div>
+      <div id="map" className="map-container" style={{ height: "100vh" }}></div>
+    </div>
+  );
+}
+
+export default Map;
