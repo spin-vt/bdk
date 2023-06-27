b755326c50121942d1fb61c2b2275b7db454190e
ZhuoweiWen <wzhuo17@gmail.com>
2023-06-27 15:24:14 -0400

    Style update for toolbar

--- front-end/components/Map.js
+++ front-end/components/Map.js
@@ -9,38 +9,45 @@ import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp
 
 const useStyles = makeStyles({
   expandToolbarButton: {
-    top: '20px',
-    position: 'absolute',  // adjust as needed
+    top: '50%',
+    position: 'absolute',
     minHeight: '6vh',
-    left: '50%',  // center the Toolbar
-    transform: "translateX(-50%)",
+    left: '20px', 
     zIndex: 1000,
     backgroundColor: '#0691DA',
     border: '0px',
-    color: '#fff', // white text
+    color: '#fff',
     '&:hover': {
       backgroundColor: '#73A5C6',
     },
     borderRadius: '30px',
     paddingLeft: '20px',
     paddingRight: '20px',
-
+    transform: `translateY(-50%)`,
+  },
+  wrapper: {
+    position: 'absolute',
+    left: '30px',
+    top: '50%',
+    transform: `translateY(-50%)`,
+    zIndex: 1000,
+    display: 'grid',
+    alignItems: 'column', // this will align items vertically in the center
+    justifyContent: 'center',
+    maxHeight: '50vh',
+    maxWidth: "10vw",
   },
   collapseToolbarContainer: {
     display: 'flex',
     alignItems: 'center', // this will align items vertically in the center
     justifyContent: 'center',
-    top: '85px',
-    position: 'absolute',  // adjust as needed
-    minHeight: '3vh',
-    maxWidth: '8vw',
-    left: '50%',  // center the Toolbar
-    transform: "translateX(-50%)",
+    // maxHeight: '5vh',
+    // maxWidth: ' 8vw',
     zIndex: 1000,
-    backgroundColor: '#3A7BD5', // Material-UI secondary color
-    color: '#fff', // white text
+    backgroundColor: '#3A7BD5',
+    color: '#fff',
     '&:hover': {
-      backgroundColor: '#73A5C6', // darker shade for hover state
+      backgroundColor: '#73A5C6',
     },
     border: '0px',
     borderRadius: '10px',
@@ -48,18 +55,17 @@ const useStyles = makeStyles({
     paddingRight: '10px',
   },
   toolbar: {
-    position: 'absolute',  // adjust as needed
-    maxHeight: '5vh',
-    left: '50%',  // center the Toolbar
-    transform: "translateX(-50%)",
-    width: "50vw",
-    borderRadius: '50px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start', 
+    justifyContent: 'center',
+    // maxHeight: '10vh',
+    // maxWidth: "20vw", // reduce width
+    borderRadius: '20px',
     backgroundColor: 'rgba(255, 255, 255, 0.8)',
     zIndex: 1000,
-    top: '20px',
-    justifyContent: 'center',
-
-  }
+  },
+  
 });
 
 
@@ -219,14 +225,14 @@ function Map({ markers }) {
 
   return (
     <div>
-      <div className={classes.toolbarContainer}>
+      <div>
         {showExpandButton && (
           <button className={classes.expandToolbarButton} onClick={() => { setIsToolbarExpanded(true); setShowExpandButton(false); }}>
             Show Toolbar
           </button>
         )}
         {isToolbarExpanded && (
-          <div >
+          <div className={classes.wrapper}>
             <Toolbar className={classes.toolbar}>
               <FormControlLabel
                 control={<Switch checked={showServed} onChange={handleServedChange} />}
