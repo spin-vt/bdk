786a5027ff7e7ac7bbee5af72c02e224796c8b4a
Vineet Marri <vineet.marri03@gmail.com>
2023-07-07 06:27:20 -0400

    modularized code and adding proper async processing

---
+++ back-end/controllers/celery_controller/controller.py
@@ -0,0 +1,88 @@
+import logging
+import subprocess
+import os
+import json
+import uuid
+from flask import jsonify
+from .celery_config import celery
+from database_op import vt_ops, fabric_ops, kml_ops
+from celery_setup.celery_tasks import process_input_file, provide_kml_locations
+
+@celery.task(bind=True, autoretry_for=(Exception,), retry_backoff=True)
+def process_input_file(self, file_name, task_id):
+    result = fabric_ops.write_to_db(file_name)
+    self.update_state(state='PROCESSED')
+    return result
+
+@celery.task(bind=True, autoretry_for=(Exception,), retry_backoff=True)
+def process_data(files, file_data_list): 
+    fabricName = ""
+    flag = False
+    names = []
+    geojson_array = []
+    tasks = []  
+
+    for file, file_data_str in zip(files, file_data_list):
+        file_name = file.filename
+        names.append(file_name)
+
+        file_data = json.loads(file_data_str)
+
+        downloadSpeed = file_data.get('downloadSpeed', '')
+        uploadSpeed = file_data.get('uploadSpeed', '')
+        techType = file_data.get('techType', '')
+        networkType = file_data.get('networkType', '')
+
+        if file_name.endswith('.csv'):
+            file.save(file_name)
+            fabricName = file_name
+
+            task_id = str(uuid.uuid4())
+
+            task = process_input_file.apply_async(args=[file_name, task_id])
+            tasks.append(task)
+
+        elif file_name.endswith('.kml'):
+            if not fabric_ops.check_num_records_greater_zero():
+                return False
+            
+            file.save(file_name)
+            task_id = str(uuid.uuid4())  
+
+            if networkType == "Wired": 
+                networkType = 0
+            else: 
+                networkType = 1
+
+            task = provide_kml_locations.apply_async(args=[fabricName, file_name, downloadSpeed, uploadSpeed, techType, flag, networkType])
+            tasks.append(task)
+            flag = True
+            kmlfile_path = os.path.join(os.getcwd(), file_name)
+            geojson_array.append(vt_ops.read_kml(kmlfile_path))
+        
+    vt_ops.create_tiles(geojson_array)
+    for name in names:
+        os.remove(name)
+
+    return jsonify({'Status': "Ok"}), 200
+
+@celery.task(bind=True)
+def provide_kml_locations(self, fabric, network, downloadSpeed, uploadSpeed, techType, flag, networkType):
+    try:
+        result = kml_ops.add_network_data(fabric, network, flag, downloadSpeed, uploadSpeed, techType, networkType)
+        self.update_state(state='PROCESSED')
+        return result
+    except Exception as e:
+        logging.exception("Error processing KML file: %s", str(e))
+        self.update_state(state='FAILED')
+        raise
+
+@celery.task(bind=True, autoretry_for=(Exception,), retry_backoff=True)
+def run_tippecanoe(self, command):
+    result = subprocess.run(command, shell=True, check=True, stderr=subprocess.PIPE)
+
+    if result.stderr:
+        print("Tippecanoe stderr:", result.stderr.decode())
+    
+    return result.returncode  # return the return code of the subprocess command
+
--- back-end/routes.py
+++ back-end/routes.py
@@ -1,16 +1,10 @@
 import logging
-import time
 import os
-import json
-import uuid
 from logging.handlers import RotatingFileHandler
 from logging import getLogger
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask import Flask, abort, jsonify, request, make_response, send_file
+from werkzeug.security import check_password_hash
+from flask import jsonify, request, make_response, send_file
 from flask_jwt_extended.exceptions import NoAuthorizationError
-from flask_jwt_extended.exceptions import JWTExtendedException
-from werkzeug.exceptions import HTTPException
-from flask_cors import CORS
 from flask_jwt_extended import (
     JWTManager,
     create_access_token,
@@ -21,14 +15,9 @@ from flask_jwt_extended.exceptions import NoAuthorizationError
 from flask import Response
 import base64
 from utils.settings import DATABASE_URL
-from database_op import fabric_ops
-from database_op import kml_ops
-from database_op import user_ops
-from database_op import vt_ops
+from database_op import fabric_ops, kml_ops, user_ops, vt_ops
 from celery_setup.celery_config import app
-from celery_setup.celery_tasks import process_input_file, provide_kml_locations
-
-
+from controllers import controller
 
 logging.basicConfig(level=logging.DEBUG)
 console_handler = logging.StreamHandler()
@@ -45,7 +34,6 @@ logger.addHandler(file_handler)
 
 logger.addHandler(console_handler)
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
 logging.basicConfig(level=logging.DEBUG)
@@ -54,8 +42,6 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'ADFAKJFDLJEOQRIOPQ498689780'
 app.config["JWT_SECRET_KEY"] = base64.b64encode(os.getenv('JWT_SECRET', 'ADFAKJFDLJEOQRI').encode())  # Use a default value for development if JWT_SECRET environment variable doesn't exist
 jwt = JWTManager(app)
 
-
-
 @app.route("/served-data", methods=['GET'])
 def get_number_records():
     return jsonify(kml_ops.get_wired_data())
@@ -63,88 +49,20 @@ def get_number_records():
 @app.route('/submit-data', methods=['POST', 'GET'])
 def submit_data():
     if request.method == 'POST':
-        names = []
-
         if 'file' not in request.files:
-            return make_response('Error: No file uploaded', 400)
+            jsonify({'Status': "Failed, no file uploaded"}), 400
 
         files = request.files.getlist('file')
 
         if len(files) <= 0:
-            return make_response('Error: No file uploaded', 400)
+            jsonify({'Status': "Failed, no file uploaded"}), 400
 
         file_data_list = request.form.getlist('fileData')
-        
-        fabricName = ""
-        flag = False
-
-        geojson_array = []
-
-        for file, file_data_str in zip(files, file_data_list):
-            file_name = file.filename
-            names.append(file_name)
-
-            file_data = json.loads(file_data_str)
-
-            downloadSpeed = file_data.get('downloadSpeed', '')
-            uploadSpeed = file_data.get('uploadSpeed', '')
-            techType = file_data.get('techType', '')
-            networkType = file_data.get('networkType', '')
-
-            if file_name.endswith('.csv'):
-                file.save(file_name)
-                fabricName = file_name
-
-                task_id = str(uuid.uuid4())
-
-                task = process_input_file.apply_async(args=[file_name, task_id])
-
-                while not task.ready():
-                    time.sleep(1)
-
-                response_data = {'Status': "Ok"}
-
-            elif file_name.endswith('.kml'):
-                if not fabric_ops.check_num_records_greater_zero():
-                    return make_response('Error: Fabric records not in database', 400)
-                
-
-                file.save(file_name)
-                task_id = str(uuid.uuid4())  
-
-                if networkType == "Wired": 
-                    networkType = 0
-                else: 
-                    networkType = 1
-
-                task = provide_kml_locations.apply_async(args=[fabricName, file_name, downloadSpeed, uploadSpeed, techType, flag, networkType])
-                logging.info("Started KML processing task with ID %s %s %s", task_id, fabricName, file_name)
-
-                while not task.ready():
-                    time.sleep(1)
-
-                logging.info("KML processing task %s completed", task_id)
-                flag = True
-                # result = task.result
-                # dict_values = result
-                kmlfile_path = os.path.join(os.getcwd(), file_name)
-                geojson_array.append(vt_ops.read_kml(kmlfile_path))
-
-                if task is False:
-                    logging.error("KML processing task %s failed with error: %s", task_id, task.traceback)
-                    response_data = {'Status': 'Error'}
-                else:
-                    response_data = {'Status': 'Ok'}
-                    
-        vt_ops.create_tiles(geojson_array)
-        for name in names:
-            os.remove(name)
-
-        return json.dumps(response_data)
+        controller.process_data.apply_async(files, file_data_list)
+        return jsonify({'Status': "OK"}), 200
 
     else:
-        response_data = {'Status': "Not a valid request type"}
-        return json.dumps(response_data)
+        jsonify({'Status': "Failed, not valid request type"}), 400
 
 
 @app.route('/')
