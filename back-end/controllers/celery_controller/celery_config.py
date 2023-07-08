e48d1cba572f75d3bebf36bb65ea2f70e155d8bb
Vineet Marri <vineet.marri03@gmail.com>
2023-07-07 22:17:38 -0400

    fixed core logic of the progress circle, still needs ux touches

--- back-end/controllers/celery_controller/celery_config.py
+++ back-end/controllers/celery_controller/celery_config.py
@@ -5,11 +5,4 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# For production
-# celery = Celery(app.name, broker='redis://bdk-redis-1:6379/0')
-# app.config['CELERY_RESULT_BACKEND'] = 'redis://bdk-redis-1:6379/0'
-# celery.conf.update(app.config)
-# You can adjust this to switch between environments as needed
-celery = Celery('my_celery_app', broker='redis://localhost:6379/0', include=['controllers.celery_controller.celery_tasks'])
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
-celery.conf.update(app.config)
+celery = Celery('my_celery_app', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0', include=['controllers.celery_controller.celery_tasks'])
