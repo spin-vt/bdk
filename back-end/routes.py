ffb3560f1e1da63781d9c7409ceb07f9be169dfc
ZhuoweiWen <wzhuo17@gmail.com>
2023-07-19 22:21:37 -0400

    Docker port expose config for server
    Signed-off-by: ZhuoweiWen <wzhuo17@gmail.com>

--- back-end/routes.py
+++ back-end/routes.py
@@ -295,7 +295,7 @@ def delete_files(fileid):
 
 # For production
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 # For local
 # if __name__ == '__main__':
