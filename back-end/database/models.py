5ca52126e6f9ebf7c408528cdb95537a7b226906
ZhuoweiWen <wzhuo17@gmail.com>
2023-09-21 14:05:37 -0400

    Added copy methods for models such that when exporting a file we can create a deepcopy of all current files in the database
    Signed-off-by: ZhuoweiWen <wzhuo17@gmail.com>

--- back-end/database/models.py
+++ back-end/database/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, Integer, String, Float
 from database.base import Base
 from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
+from datetime import datetime
 
 class user(Base):
     __tablename__ = 'user'
@@ -26,6 +27,23 @@ class folder(Base):
     mbtiles = relationship('mbtiles', back_populates='folder', cascade='all, delete')
     kmzs = relationship('kmz', back_populates='folder', cascade='all, delete')
 
+    def copy(self, session, name=None, type=None):
+        name = name if name is not None else self.name
+        type = type if type is not None else self.type
+        new_folder = folder(name=name, type=type, user_id=self.user_id)
+        session.add(new_folder)
+        session.flush()  # To generate an ID for the new folder
+        
+        # Copy related files, mbtiles, and kmzs
+        for file in self.files:
+            file.copy(session=session, new_folder_id=new_folder.id)
+        for mbtile in self.mbtiles:
+            mbtile.copy(session=session, new_folder_id=new_folder.id)
+        for kmz in self.kmzs:
+            kmz.copy(session=session, new_folder_id=new_folder.id)
+
+        return new_folder
+
 class kmz(Base):
     __tablename__ = 'kmz'
     
@@ -34,6 +52,15 @@ class kmz(Base):
     folder_id = Column(Integer, ForeignKey('folder.id', ondelete='CASCADE'))
     kml_files = relationship('file', back_populates='kmz', cascade='all, delete')  # Assuming kml_data is your KML model
     folder = relationship('folder', back_populates='kmzs')  # Add this new back_populates to your Folder model
+    def copy(self, session, new_folder_id):
+        new_kmz = kmz(name=self.name, folder_id=new_folder_id)
+        session.add(new_kmz)
+        session.flush()
+        
+        for kml_file in self.kml_files:
+            kml_file.copy(session=session, new_kmz_id=new_kmz.id, new_folder_id=new_folder_id)
+
+        return new_kmz
 
 class file(Base):
     __tablename__ = 'file'
@@ -50,6 +77,68 @@ class file(Base):
     folder = relationship('folder', back_populates='files')
     fabric_data = relationship('fabric_data', back_populates='file', cascade='all, delete')  # Use fabric_data instead of data_entries
     kml_data = relationship('kml_data', back_populates='file', cascade='all, delete')
+
+    def copy(self, session, new_kmz_id=None, new_folder_id=None):
+        new_file = file(name=self.name, 
+                        data=self.data, 
+                        folder_id=new_folder_id, 
+                        kmz_id=new_kmz_id, 
+                        timestamp=datetime.now(), 
+                        type=self.type, 
+                        computed=self.computed)
+        session.add(new_file)
+        session.flush()
+        
+        fabric_data_copies = []
+        for fabric_entry in self.fabric_data:
+            fabric_data_copy = {
+                "location_id":fabric_entry.location_id,
+                "address_primary" : fabric_entry.address_primary,
+                "city" : fabric_entry.city,
+                "state" : fabric_entry.state,
+                "zip_code" : fabric_entry.zip_code,
+                "zip_suffix" : fabric_entry.zip_suffix,
+                "unit_count" : fabric_entry.unit_count,
+                "bsl_flag" : fabric_entry.bsl_flag,
+                "building_type_code" : fabric_entry.building_type_code,
+                "land_use_code" : fabric_entry.land_use_code,
+                "address_confidence_code" : fabric_entry.address_confidence_code,
+                "country_geoid" : fabric_entry.country_geoid,
+                "block_geoid" : fabric_entry.block_geoid,
+                "h3_9" : fabric_entry.h3_9,
+                "latitude" : fabric_entry.latitude,
+                "longitude" : fabric_entry.longitude,
+                "fcc_rel" : fabric_entry.fcc_rel,
+                "file_id" : new_file.id
+            }
+            fabric_data_copies.append(fabric_data_copy)
+
+        kml_data_copies = []
+        for kml_entry in self.kml_data:
+            kml_data_copy = {
+                "location_id":kml_entry.location_id,
+                "served" : kml_entry.served,
+                "wireless" : kml_entry.wireless,
+                "lte" : kml_entry.lte,
+                "username" : kml_entry.username,
+                "coveredLocations" : kml_entry.coveredLocations,
+                "maxDownloadNetwork" : kml_entry.maxDownloadNetwork,
+                "maxDownloadSpeed" : kml_entry.maxDownloadSpeed,
+                "maxUploadSpeed" : kml_entry.maxUploadSpeed,
+                "techType" : kml_entry.techType,
+                "address_primary" : kml_entry.address_primary,
+                "longitude" : kml_entry.longitude,
+                "latitude" : kml_entry.latitude,
+                "latency" : kml_entry.latency,
+                "category" : kml_entry.category,
+                "file_id": new_file.id
+            }
+            kml_data_copies.append(kml_data_copy)
+
+        session.bulk_insert_mappings(fabric_data, fabric_data_copies)
+        session.bulk_insert_mappings(kml_data, kml_data_copies)
+
+        return new_file
 
 class fabric_data(Base):
     __tablename__ = 'fabric_data'
@@ -73,6 +162,30 @@ class fabric_data(Base):
     file_id = Column(Integer, ForeignKey('file.id', ondelete='CASCADE'))
     id = Column(Integer, primary_key=True, autoincrement=True)  # Unique primary key
     file = relationship('file', back_populates='fabric_data')
+
+    # def copy(self, session, new_file_id):
+    #     new_fabric_data = fabric_data(
+    #         location_id=self.location_id,
+    #         address_primary = self.address_primary,
+    #         city = self.city,
+    #         state = self.state,
+    #         zip_code = self.zip_code,
+    #         zip_suffix = self.zip_suffix,
+    #         unit_count = self.unit_count,
+    #         bsl_flag = self.bsl_flag,
+    #         building_type_code = self.building_type_code,
+    #         land_use_code = self.land_use_code,
+    #         address_confidence_code = self.address_confidence_code,
+    #         country_geoid = self.country_geoid,
+    #         block_geoid = self.block_geoid,
+    #         h3_9 = self.h3_9,
+    #         latitude = self.latitude,
+    #         longitude = self.longitude,
+    #         fcc_rel = self.fcc_rel,
+    #         file_id=new_file_id
+    #     )
+    #     session.add(new_fabric_data)
+    #     return new_fabric_data
 
     # __table_args__ = (UniqueConstraint('location_id', 'user_id', name='location_user_uc'),)
 
@@ -118,6 +231,28 @@ class kml_data(Base):
     latency = Column(Integer)
     category = Column(String)
 
+    # def copy(self, session, new_file_id):
+    #     new_kml_data = kml_data(
+    #         location_id=self.location_id,
+    #         served = self.served,
+    #         wireless = self.wireless,
+    #         lte = self.lte,
+    #         username = self.username,
+    #         coveredLocations = self.coveredLocations,
+    #         maxDownloadNetwork = self.maxDownloadNetwork,
+    #         maxDownloadSpeed = self.maxDownloadSpeed,
+    #         maxUploadSpeed = self.maxUploadSpeed,
+    #         techType = self.techType,
+    #         address_primary = self.address_primary,
+    #         longitude = self.longitude,
+    #         latitude = self.latitude,
+    #         latency = self.latency,
+    #         category = self.category,
+    #         file_id=new_file_id
+    #     )
+    #     session.add(new_kml_data)
+    #     return new_kml_data
+
 class mbtiles(Base):
     __tablename__ = 'mbtiles'
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -128,6 +263,26 @@ class mbtiles(Base):
     folder = relationship('folder', back_populates='mbtiles')
     vector_tiles = relationship('vector_tiles', back_populates='mbtiles', cascade='all, delete')
 
+    def copy(self, session, new_folder_id):
+        new_mbtile = mbtiles(filename=self.filename, 
+                              tile_data=self.tile_data, 
+                              timestamp=datetime.now(), 
+                              folder_id=new_folder_id)
+        session.add(new_mbtile)
+        session.flush()
+
+        vector_tile_copies = []
+        for vector_tile in self.vector_tiles:
+            vector_tile_copy = {
+                "zoom_level" : vector_tile.zoom_level,
+                "tile_column" : vector_tile.tile_column,
+                "tile_row" : vector_tile.tile_row,
+                "tile_data" : vector_tile.tile_data,
+                "mbtiles_id" : new_mbtile.id
+            }
+            vector_tile_copies.append(vector_tile_copy)
+        session.bulk_insert_mappings(vector_tiles, vector_tile_copies)
+
 class vector_tiles(Base):
     __tablename__ = 'vector_tiles'
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -137,6 +292,18 @@ class vector_tiles(Base):
     tile_data = Column(LargeBinary)
     mbtiles_id = Column(Integer, ForeignKey('mbtiles.id', ondelete='CASCADE'))
     mbtiles = relationship('mbtiles', back_populates='vector_tiles')
+
+    # def copy(self, session, new_mbtile_id):
+    #     new_vector_tile = vector_tiles(
+    #         zoom_level = self.zoom_level,
+    #         tile_column = self.tile_column,
+    #         tile_row = self.tile_row,
+    #         tile_data = self.tile_data,
+    #         mbtiles_id = new_mbtile_id
+
+    #     )
+    #     session.add(new_vector_tile)
+    #     return new_vector_tile
 
 class ChallengeLocations(Base):
     __tablename__ = 'challenge_locations'
