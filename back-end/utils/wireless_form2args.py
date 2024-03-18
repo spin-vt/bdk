44424a3f38ff46be7e0277032bcb070771984e97
shabanii2023F <shabanic@aol.com>
2024-03-18 15:27:19 -0400

    added extra arguments

--- back-end/utils/wireless_form2args.py
+++ back-end/utils/wireless_form2args.py
@@ -6,8 +6,16 @@ wireless_formfield2args = {
     'antennaHeight': '-txh',
     'antennaTilt': '-dt',
     'horizontalFacing': '-rot',
+    'effectiveRadPower': '-erp',
+    'downtilt': '-dt',
+    'downtiltDirection': '-dtdir'
     # ... additional mappings
+    # for th rest below, on the front end
+    # these can be buttons that select the options
+    
 }
 
 wireless_raster_file_format = ['.kmz', '.lcf', '.png', '.ppm']
 wireless_vector_file_format = ['.kml', '.png', '.tif']
+
+# runsig.sh -sdf /app/DEM/one_arc_second -erp 1000.0 -lat 46.37980555555556 -lon -116.14330555555556 -f 5785 -txh 54.8 -rot 330 -dt 2 -dtdir 100 -R 10 -pm 1 -res 3600 -o output | genkmz.sh
