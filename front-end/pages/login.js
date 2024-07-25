b054004497bcf2a9680208ffe2be2868050562f5
ZhuoweiWen <wzhuo17@gmail.com>
2024-07-25 18:49:38 -0400

    Remove firebase
    Signed-off-by: ZhuoweiWen <wzhuo17@gmail.com>

--- front-end/pages/login.js
+++ front-end/pages/login.js
@@ -4,12 +4,8 @@ import { TextField, Button, Typography, Container } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Swal from 'sweetalert2';
 import { backend_url } from '../utils/settings';
-import { auth, GoogleAuthProvider, EmailAuthProvider } from "../firebase/firebaseConfig";
-import "firebaseui/dist/firebaseui.css";
 import styles from '../styles/Login.module.css';
 import Navbar from '../components/Navbar';
-
-let firebaseui;
 
 const LoginContainer = styled("div")({
   display: "flex",
@@ -52,7 +48,7 @@ const Divider = styled("div")({
 
 const Login = () => {
   const router = useRouter();
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = async (e) => {
@@ -64,7 +60,7 @@ const Login = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ username, password }),
         credentials: 'include',
       });
 
@@ -74,7 +70,7 @@ const Login = () => {
           router.push('/');
         } else {
           if (data.message === 'Invalid credentials') {
-            Swal.fire('Error', 'Incorrect email or password.', 'error');
+            Swal.fire('Error', 'Incorrect username or password.', 'error');
           }
         }
       } else {
@@ -92,67 +88,7 @@ const Login = () => {
     router.push('/register');
   };
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      firebaseui = require('firebaseui');
-      const uiConfig = {
-        signInSuccessUrl: "/",
-        signInOptions: [
-          GoogleAuthProvider.PROVIDER_ID,
-          EmailAuthProvider.PROVIDER_ID,
-        ],
-        callbacks: {
-          signInSuccessWithAuthResult: async (authResult) => {
-            if (typeof window !== "undefined") {
-
-              // Extracting user information
-              const { user } = authResult;
-              const username = user.email; // Using email as the username
-              const password = user.uid;   // Using uid as a placeholder password
-
-              // Send this information to your backend
-              try {
-                const response = await fetch(`${backend_url}/api/register`, {
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify({ username, password }),  // send them as required by your backend
-                  credentials: 'include',
-                });
-
-                if (response.ok) {
-                  const data = await response.json();
-                  if (data.status !== 'success') {
-                    Swal.fire('Error', data.message || 'Unknown error occurred during registration.', 'error');
-                  }
-                } else {
-                  Swal.fire('Error', 'Failed to register with Firebase.', 'error');
-                }
-
-              } catch (error) {
-                Swal.fire('Error', 'Failed to register with Firebase.', 'error');
-                console.error('Firebase registration error:', error);
-              }
-
-              window.location.href = "/";
-            }
-            return false;
-          },
-        },
-      };
-
-      let ui = firebaseui.auth.AuthUI.getInstance();
-      if (!ui) {
-        ui = new firebaseui.auth.AuthUI(auth);
-      }
-      ui.start("#firebaseui-auth-container", uiConfig);
-
-      return () => {
-        ui.delete();
-      };
-    }
-  }, []);
+  
 
   return (
     <div>
@@ -169,14 +105,13 @@ const Login = () => {
                 margin="normal"
                 required
                 fullWidth
-                id="email"
-                label="Email"
-                name="email"
-                autoComplete="email"
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
                 autoFocus
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                key="email-input"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
               />
               <TextField
                 variant="outlined"
@@ -208,24 +143,9 @@ const Login = () => {
                   Register
                 </RegisterButton>
               </LoginButtonContainer>
-              <Button
-                fullWidth
-                variant="text"
-                color="primary"
-                onClick={() => router.push('/resetPassword')}
-                style={{ marginTop: '1rem' }}
-              >
-                Forgot Password?
-              </Button>
             </LoginForm>
           </LoginContainer>
 
-          <Divider></Divider>
-          <div id="firebaseui-auth-container"></div>
-
-          <div className={styles.container}>
-            <div id="firebaseui-auth-container"></div>
-          </div>
         </AuthContainer>
       </Container>
     </div>
